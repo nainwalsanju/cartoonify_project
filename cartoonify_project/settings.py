217e37f89ea2ded80b2283c173cd783310822d26
sanjay <sanjaynainwal129@gmail.com>
2023-06-10 04:07:57 +0530

    update

--- cartoonify_project/settings.py
+++ cartoonify_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m6b2zydk7+2nu8+7lig38*ibv!&6$p@(7@*=krp^%-%ahgaaq%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
